a3a44f7d8b74256df2686016a2ece116059d3cd9
bilisimfatihi <bilisimfatihi@gmail.com>
2026-02-08 22:38:49 +0300

    chore: remove firestore seed trigger from ui

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,5 @@
-import { seedPsychologists } from "../../scripts/seedPsychologists";
-
 const Home = () => {
-  return (
-    <div className="p-4">
-      <button
-        onClick={seedPsychologists}
-        className="rounded bg-blue-600 px-4 py-2 text-white"
-      >
-        Seed Psychologists (DEV)
-      </button>
-    </div>
-  );
+  return <h2 className="text-2xl font-semibold">Home Page</h2>;
 };
 
 export default Home;
