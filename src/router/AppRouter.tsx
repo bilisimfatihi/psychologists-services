3f999b9dfe2388fb9280c03eae37d329e913d964
bilisimfatihi <bilisimfatihi@gmail.com>
2026-02-08 19:46:33 +0300

    feat: add protected routes based on auth state

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,22 +1,30 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import Layout from "../components/Layout"
-import Psychologists from "../pages/Psychologists"
-import Favorites from "../pages/Favorites"
-import Home from "../pages/Home"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
+import Layout from "../components/Layout";
+import Psychologists from "../pages/Psychologists";
+import Favorites from "../pages/Favorites";
+import Home from "../pages/Home";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Layout />} >
-                <Route index element={<Home />} />
-                <Route path="psychologists" element={<Psychologists />} />
-                <Route path="favorites" element={<Favorites />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </Route>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="psychologists" element={<Psychologists />} />
+          <Route
+            path="favorites"
+            element={
+              <ProtectedRoute>
+                <Favorites />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
---
+++ src/router/ProtectedRoute.tsx
@@ -0,0 +1,22 @@
+import { Navigate } from "react-router-dom";
+import type { ReactNode } from "react";
+import { useAuth } from "../hooks/useAuth";
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <>{children}</>;
+};
+export default ProtectedRoute;
