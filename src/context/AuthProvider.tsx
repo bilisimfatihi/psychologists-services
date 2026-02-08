2ae9f50ea61d0189700f11b358a046af811743b0
bilisimfatihi <bilisimfatihi@gmail.com>
2026-02-08 16:23:44 +0300

    split AuthContext and AuthProvider

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, type User } from "@firebase/auth";
+import { auth } from "../firebase/config";
+import { AuthContext } from "./AuthContext";
+import type { ReactNode } from "react";
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
